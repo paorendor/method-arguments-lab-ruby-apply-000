a337944ab682ae245788d20d75508024ac41c1a1
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-12 19:56:42 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,11 @@
 def 
-  intrduction (name)
-  puts "Hi, my name is #{name}."
+  introduction (name)
+  puts "Hi, my name is #{name}.\n"
 end
 
 def 
   introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and Iam learning to program in #{language}."
+  puts "Hi, my name is #{name} and Iam learning to program in #{language}.\n"
  
  end
   
